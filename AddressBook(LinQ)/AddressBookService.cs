af867f77c0bb1a396892d7aa25ff71753bd1ca6e
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-07 22:50:55 +0530

    [Srushti]Add.Insert values in table

--- AddressBook(LinQ)/AddressBookService.cs
+++ AddressBook(LinQ)/AddressBookService.cs
@@ -18,6 +18,13 @@ namespace AddressBook_LinQ_
             dataTable.Columns.Add("ZipCode");
             dataTable.Columns.Add("PhoneNo");
             dataTable.Columns.Add("Email");
+
+            dataTable.Rows.Add("Seeta", "Ram", "02-Green Tower", "Mumbai", "Maharashtra", "400500", "9999999999", "seeta@gmail.com");
+            dataTable.Rows.Add("Ram", "Verma", "03-Shastri Nagar", "Delhi", "Delhi", "788890", "7777777777", "ram@gmail.com");
+            dataTable.Rows.Add("Rahul", "Yadav", "04-Orchids Colony", "Bangalore", "Karnataka","500300","4444444444", "rahulyadav@gmail.com");
+            dataTable.Rows.Add("Riya", "Singh", "05-Pancham Society", "Mumbai", "Maharashtra", "400503", "3333333333", "riya@gmail.com");
+
+            Console.WriteLine("Table created successfully");
         }
     }
 }
