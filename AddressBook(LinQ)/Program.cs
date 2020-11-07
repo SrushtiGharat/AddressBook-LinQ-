19f834cebaf1e72445abe0dfbb04d797afc2a85c
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-07 18:03:33 +0530

    Add project files.

---
+++ AddressBook(LinQ)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_LinQ_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book using Linq");
+        }
+    }
+}
