69447405a5d16c49f8b6a6f506a50c92ca644cb4
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-09 08:50:59 +0530

    [Srushti]Add.Edit contact using name

--- AddressBook(LinQ)/AddressBookService.cs
+++ AddressBook(LinQ)/AddressBookService.cs
@@ -19,12 +19,34 @@ namespace AddressBook_LinQ_
             dataTable.Columns.Add("PhoneNo");
             dataTable.Columns.Add("Email");
 
-            dataTable.Rows.Add("Seeta", "Ram", "02-Green Tower", "Mumbai", "Maharashtra", "400500", "9999999999", "seeta@gmail.com");
+            dataTable.Rows.Add("Seeta", "Rai", "02-Green Tower", "Mumbai", "Maharashtra", "400500", "9999999999", "seeta@gmail.com");
             dataTable.Rows.Add("Ram", "Verma", "03-Shastri Nagar", "Delhi", "Delhi", "788890", "7777777777", "ram@gmail.com");
             dataTable.Rows.Add("Rahul", "Yadav", "04-Orchids Colony", "Bangalore", "Karnataka","500300","4444444444", "rahulyadav@gmail.com");
             dataTable.Rows.Add("Riya", "Singh", "05-Pancham Society", "Mumbai", "Maharashtra", "400503", "3333333333", "riya@gmail.com");
 
             Console.WriteLine("Table created successfully");
+        }
+
+        public void EditPhoneNo(string name,string phoneNo)
+        {
+            var result = from contact in dataTable.AsEnumerable()
+                        where contact.Field<string>("First Name") == name
+                        select contact;
+            foreach(var contact in result)
+            {
+                contact.SetField("PhoneNo", phoneNo);
+            }
+            Console.WriteLine("Contact edited successfully");
+        }
+
+        public void Display()
+        {
+            foreach(var contact in dataTable.AsEnumerable())
+            {
+                Console.WriteLine(contact.Field<string>("First Name")+"\t"+ contact.Field<string>("Last Name")+
+                    "\t"+ contact.Field<string>("Address")+"\t"+contact.Field<string>("City")+"\t"+ contact.Field<string>("State")+
+                    "\t"+ contact.Field<string>("ZipCode")+"\t"+ contact.Field<string>("PhoneNo")+"\t"+ contact.Field<string>("Email"));
+            }
         }
     }
 }
--- AddressBook(LinQ)/Program.cs
+++ AddressBook(LinQ)/Program.cs
@@ -9,6 +9,24 @@ namespace AddressBook_LinQ_
             Console.WriteLine("Welcome to address book using Linq");
             AddressBookService addressBook = new AddressBookService();
             addressBook.CreateTable();
+
+            Console.WriteLine("1.Edit Contact");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter new phone no");
+                    string phone = Console.ReadLine();
+                    addressBook.EditPhoneNo(name, phone);
+
+                    addressBook.Display();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
         }
     }
 }
